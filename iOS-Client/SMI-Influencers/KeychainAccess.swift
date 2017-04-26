dd1fb1b958c15ef25cfcbbef47415e99f337c5d5
Kadeem Palacios <kadeempalacios@gmail.com>
2017-04-26 13:39:20 -0700

    Added Storyboard routing based on token availability to app delegate. FB ogin sets token upon login (Need refactoring)

---
+++ iOS-Client/SMI-Influencers/KeychainAccess.swift
@@ -0,0 +1,20 @@
+//
+//  KeychainAccess.swift
+//  SMI-Influencers
+//
+//  Created by Kadeem Palacios on 4/26/17.
+//  Copyright © 2017 Kadeem Palacios. All rights reserved.
+//
+
+import Foundation
+import KeychainSwift
+
+class KeychainAccess {
+    
+    
+    let keychain = KeychainSwift()
+    var token:String? = nil
+
+    
+    
+}
